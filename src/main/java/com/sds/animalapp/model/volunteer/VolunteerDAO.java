3ff0dbcd2bd025556d2c3ea5949ba3dc42ef341f
hg316 <khgn316@gmail.com>
2024-06-08 09:27:41 +0900

    봉사 파라미터 클래스

--- src/main/java/com/sds/animalapp/model/volunteer/VolunteerDAO.java
+++ src/main/java/com/sds/animalapp/model/volunteer/VolunteerDAO.java
@@ -1,17 +1,17 @@
 package com.sds.animalapp.model.volunteer;
 
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import com.sds.animalapp.domain.VolunteerNotice;
+import com.sds.animalapp.domain.VolunteerSelectParam;
 
 @Mapper
 public interface VolunteerDAO {
 	public int selectCount(String keyword); //게시물 수 
 	public int selectRegistCount(int id);
-	public List selectAll(Map map);//모든 게시물 가져오기
+	public List selectAll(VolunteerSelectParam volunteerSelectParam);//모든 게시물 가져오기
 	public VolunteerNotice select(int id);
 	public void insert(VolunteerNotice volunteer);
 	public void update(VolunteerNotice volunteer);
