8b8df3031f48ddb467604b23ad151357687457da
jaehunleee <joz523@naver.com>
2024-06-14 17:59:07 +0900

    API 변경사항만 업데이트하는 기능 구현

--- src/main/java/com/sds/animalapp/model/animal/AnimalService.java
+++ src/main/java/com/sds/animalapp/model/animal/AnimalService.java
@@ -6,19 +6,23 @@ import com.sds.animalapp.domain.Animal;
 import com.sds.animalapp.domain.AnimalSelectParam;
 
 public interface AnimalService {
-	public void insertAll(List<Animal> animalList); // 보호동물 API 데이터 insert 문
+	void insertAll(List<Animal> animalList); // 보호동물 API 데이터 insert
 
-	public void delete();
+	void insert(Animal animal); // 새로운 데이터 삽입
 
-	public int selectCount(AnimalSelectParam animalSelectParam); // 필터링에 따른 총 레코드 수
+	void update(Animal animal); // 기존 데이터 업데이트
 
-	public List selectAll(AnimalSelectParam animalSelectParam); // 필터링에 매칭되는 모든 필터링 된 동물 조회
+	void delete();
 
-	public Animal select(int animal_idx); // 동물 상세 조회
+	int selectCount(AnimalSelectParam animalSelectParam); // 필터링에 따른 총 레코드 수
 
-	public List selectPreview(); // 미리보기
+	List<Animal> selectAll(AnimalSelectParam animalSelectParam); // 필터링에 매칭되는 모든 동물 조회
 
-	// 한 동물에 대한 입양신청 인원 조회
-	public int countRegistMember(int animal_idx);
+	Animal select(int animal_idx); // 동물 상세 조회
 
+	List<Animal> selectPreview(); // 미리보기
+
+	int countRegistMember(int animal_idx); // 한 동물에 대한 입양신청 인원 조회
+
+	Animal selectByDesertionNo(String desertionNo); // 유기번호로 동물 조회
 }
