d4062d379fb78670149ba1ef3947fb6d667cb5ff
jaehunleee <joz523@naver.com>
2024-06-22 16:43:30 +0900

    봉사신청글의 보호소 매핑, 메인 UI 수정

--- src/main/java/com/sds/animalapp/domain/VolunteerNotice.java
+++ src/main/java/com/sds/animalapp/domain/VolunteerNotice.java
@@ -4,12 +4,13 @@ import lombok.Data;
 
 @Data
 public class VolunteerNotice {
-    private int vol_event_post_idx;
-    private String vol_date;
-    private int recruit_count;
-    private String shelter_name;
-    private String body_text;
-    private String regdate;
-    private String title;
-    private int member_idx;
+	private Integer shelter_idx;
+	private int vol_event_post_idx;
+	private String vol_date;
+	private int recruit_count;
+	private String shelter_name;
+	private String body_text;
+	private String regdate;
+	private String title;
+	private int member_idx;
 }
